4fa6c0c938f5c9112c3dedd67e751dc6f28d05d1
Joseph Bui <jhbui@cpp.edu>
2022-10-23 16:49:14 -0700

    Initial outline for code

---
+++ RestaurantBill.c
@@ -0,0 +1,15 @@
+//declare required variables
+
+//take in arguments from the command line
+int main()
+{
+    //create a randomizer for the number of items in each bill.
+
+    //print out each item and price
+
+    //store command line arguments in to variables
+
+    //calculate the tax, tip, and bill amount
+
+    //print the final values
+}

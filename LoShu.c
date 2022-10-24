b4931daffb1876e6a4f37832b64cf8a17052df1a
Joseph Bui <jhbui@cpp.edu>
2022-10-23 17:54:42 -0700

    Initial file with code outline

---
+++ LoShu.c
@@ -0,0 +1,15 @@
+//create a function that adds up all the columns and rows of the Lo Shu board
+
+//create a functoin that prints out the board
+
+
+int main()
+{
+    // create a random number generator
+
+    //create a 3 by 3 array
+
+    //create a loop that creates Lo Shu game boards until the magic square is found
+
+
+}
